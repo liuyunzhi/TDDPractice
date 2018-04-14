54448ab2876740ceece9bca8a4c7ef5fb7232d9c
刘云志 <liuyunzhi@chinamcloud.com>
2018-04-14 13:29:59 +0800

    完成练习

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,14 +5,61 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
-    private List<String> results = new ArrayList<>();
+    private List<String> results;
+    private int count;
+    private int firstSpecialNumber;
+    private int secondSpecialNumber;
+    private int thirdSpecialNumber;
 
+    public FizzBuzzGame() {
+        results = new ArrayList<>();
+        count = 0;
+        firstSpecialNumber = 3;
+        secondSpecialNumber = 5;
+        thirdSpecialNumber = 7;
+    }
+
+    public void setSpecialNumber(int firstSpecialNumber, int secondSpecialNumber, int thirdSpecialNumber) {
+        this.firstSpecialNumber = firstSpecialNumber;
+        this.secondSpecialNumber = secondSpecialNumber;
+        this.thirdSpecialNumber = thirdSpecialNumber;
+    }
+
+    private void handle() {
+        for (int i = 1; i <= count; i++) {
+            String value = String.valueOf(i);
+            if (value.contains(String.valueOf(firstSpecialNumber))) {
+                results.add("Fizz");
+            } else if(i % firstSpecialNumber == 0 && i % secondSpecialNumber == 0 && i % thirdSpecialNumber == 0) {
+                results.add("FizzBuzzWhizz");
+            } else if (i % firstSpecialNumber == 0 && i % secondSpecialNumber == 0) {
+                results.add("FizzBuzz");
+            } else if (i % firstSpecialNumber == 0 && i % thirdSpecialNumber == 0) {
+                results.add("FizzWhizz");
+            } else if (i % secondSpecialNumber == 0 && i % thirdSpecialNumber == 0) {
+                results.add("BuzzWhizz");
+            } else if (i % firstSpecialNumber == 0) {
+                results.add("Fizz");
+            } else if (i % secondSpecialNumber == 0) {
+                results.add("Buzz");
+            } else if (i % thirdSpecialNumber == 0) {
+                results.add("Whizz");
+            } else {
+                results.add(String.valueOf(i));
+            }
+        }
+    }
     public void start(int count) {
-
+        this.count = count;
+        handle();
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    public String getResultByIndex(int index) {
+        return results.get(index - 1);
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,23 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class FizzBuzzGameTest {
     @Test
     public void should_return_Fizz_when_have_3() {
+        FizzBuzzGame game = new FizzBuzzGame();
+        game.start(100);
 
+        String resultOf3 = game.getResultByIndex(3);
+        String resultOf13 = game.getResultByIndex(13);
+        String resultOf35 = game.getResultByIndex(35);
+
+        assertThat(resultOf3, equalTo("Fizz"));
+        assertThat(resultOf13, equalTo("Fizz"));
+        assertThat(resultOf35, equalTo("Fizz"));
     }
 }
